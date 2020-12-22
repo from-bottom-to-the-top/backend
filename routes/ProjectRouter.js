c612513874e29872a9f3e594cc92bb071db759ee
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2020-12-22 01:18:59 +0100

    project refactory

--- routes/ProjectRouter.js
+++ routes/ProjectRouter.js
@@ -2,6 +2,6 @@ import express from "express";
 import projectController from "../controller/projectController";
 const router = express.Router();
 
-router.get("/", projectController.index);
-router.post("/add", projectController.create);
+router.get("/", projectController.index)
+      .post("/add", projectController.create);
 export default router;
